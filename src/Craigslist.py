0a35d0f7dbad96de71b138a683c425003fc3bf8d
hussain.ujjaini <husain.ujjaini72@gmail.com>
2018-12-11 17:41:43 +0530

    "updated Craigslist.py with basic function"

--- src/Craigslist.py
+++ src/Craigslist.py
@@ -1,3 +1,30 @@
 import json
 
 class Craigslist:
+
+    def basic(self):
+        with open("../data/json_data.json", "r") as read_file:
+            data = json.load(read_file)
+            id = []
+            loc = []
+            userId = []
+            description = []
+            price = []
+            status = []
+            for inputs in data:
+                id.append(inputs["id"])
+                loc.append(inputs["loc"])
+                userId.append(inputs["userId"])
+                description.append(inputs["description"])
+                price.append(inputs["price"])
+                status.append(inputs["status"])
+
+	def getItemByStatus(self, status):
+		return None
+
+    def getItemsByUserId(self, userid):
+        return None
+
+if __name__ == '__main__':
+    c = Craigslist()
+    c.basic()
